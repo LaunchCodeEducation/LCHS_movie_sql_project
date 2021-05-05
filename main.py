9850e56fbcd524287a8908928159877eb9cc9081
Jim Flores <jimflores5@gmail.com>
2021-05-05 17:39:35 -0500

    Configured templates to starting point.

--- main.py
+++ main.py
@@ -5,110 +5,35 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.secret_key = 'K>~EEAnH_x,Z{q.43;NmyQiNz1^Yr7'
 
-def execute_query(query_string):
-    db = sqlite3.connect('project.db')
-    cursor = db.cursor()
-    if "select" in query_string.lower():
-        try:
-            results = list(cursor.execute(query_string))
-        except:
-            results = 'error'
-    else:
-        try:
-            cursor.execute(query_string)
-            db.commit()
-            results = "success"
-        except:
-            results = 'error'
-    db.close()
-    return results
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         query_type = '/' + request.form['query_type'].lower()
         session['table'] = request.form['table']
         if session['table'] == 'movies':
-            session['columns'] = [['movie_id', 'number'], ['title', 'text'], ['year_released', 'number'], ['director', 'text']]
+            session['columns'] = ['movie_id', 'title', 'year_released', 'director']
         else:
-            session['columns'] = [['director_id', 'number'], ['last_name', 'text'], ['first_name', 'text']]
+            session['columns'] = ['director_id', 'last_name', 'first_name']
         return redirect(query_type)
-
-    query_types = ['INSERT', 'SELECT', 'UPDATE', 'DELETE']
-    return render_template("index.html", tab_title = "Movie SQL Project", query_types = query_types)
-
-@app.route('/select', methods=['GET', 'POST'])
-def select_query():
-    if request.method == 'POST':
-        table = session['table']
-        columns = request.form['columns']
-        condition = request.form['condition']
-        if len(columns) == 0 or '*' in columns:
-            columns = '*'
-            if table == 'movies':
-                session['selected_columns'] = ['movie_id', 'title', 'year_released', 'director']
-            else:
-                session['selected_columns'] = ['director_id', 'last_name', 'first_name']
-        else:
-            session['selected_columns'] = columns.split(',')
-        sql_query = f"SELECT {columns} FROM {table}"
-        if condition != '':
-            sql_query += f" WHERE {condition}"
-        results = execute_query(sql_query)
     else:
-        sql_query = ''
-        results = []
-
-    return render_template("select.html", tab_title = "Movie SQL Project", sql_query = sql_query, results = results)
+        query_types = ['INSERT', 'SELECT', 'UPDATE', 'DELETE']
+    return render_template("index.html", tab_title = "Movie SQL Project", query_types = query_types, home=True)
 
 @app.route('/insert', methods=['GET', 'POST'])
 def insert_query():
-    if request.method == 'POST':
-        table = session['table']
-        columns = request.form['columns']
-        values = request.form['values']
-        to_enter = values.split(',')
-        for index in range(len(to_enter)):
-            to_enter[index] = to_enter[index].strip()
-            if to_enter[index].isdigit():
-                to_enter[index] = int(to_enter[index])
+    return render_template("insert.html", tab_title = "INSERT query", home=False)
 
-        sql_query = f"INSERT INTO {table} ({columns}) VALUES ({values})"
-        results = execute_query(sql_query)
-    else:
-        sql_query = ''
-        results = []
-
-    return render_template("insert.html", tab_title = "Movie SQL Project", sql_query = sql_query, results = results)
+@app.route('/select', methods=['GET', 'POST'])
+def select_query():
+    return render_template("select.html", tab_title = "SELECT query", home=False)
 
 @app.route('/update', methods=['GET', 'POST'])
 def update_query():
-    if request.method == 'POST':
-        table = session['table']
-        new_value = request.form['new_value']
-        condition = request.form['condition']
-        sql_query = f"UPDATE {table} SET {new_value}"
-        if condition != '':
-            sql_query += f" WHERE {condition}"
-        results = execute_query(sql_query)
-    else:
-        sql_query = ''
-        results = []
-
-    return render_template("update.html", tab_title = "Movie SQL Project", sql_query = sql_query, results = results)
+    return render_template("update.html", tab_title = "UPDATE query", home=False)
 
 @app.route('/delete', methods=['GET', 'POST'])
 def delete_query():
-    if request.method == 'POST':
-        table = session['table']
-        condition = request.form['condition']
-        sql_query = f"DELETE FROM {table} WHERE {condition}"
-        results = execute_query(sql_query)
-    else:
-        sql_query = ''
-        results = []
-
-    return render_template("delete.html", tab_title = "Movie SQL Project", sql_query = sql_query, results = results)
+    return render_template("delete.html", tab_title = "DELETE query", home=False)
 
 if __name__ == '__main__':
     app.run()
